595603ce670abb7d1b9f264111ffe615e2ce6ad2
hjiayz <hjiayz@163.com>
2017-11-03 12:43:22 +0800

    bind_generic

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ class RouterList {
         let i = t.index;
         let lopt = {};
         let lcomp = {};
+        let lgeneric = {};
+        let lcompid = {};
         let lresult = {};
         let lparam = {};
         let changed = false;
@@ -27,6 +29,11 @@ class RouterList {
         this.bind = (type, inputcomp, viewcomp) => {
             t.type.assert(type);
             lcomp[jsonify(type)] = [inputcomp, viewcomp];
+            changed = true;
+            return this;
+        }
+        this.bind_generic = (generic, inputcomp, viewcomp) => {
+            lgeneric[jsonify(generic)] = [inputcomp, viewcomp];
             changed = true;
             return this;
         }
@@ -41,7 +48,8 @@ class RouterList {
             if (!changed) return;
             lresult = {};
             lparam = {};
-            Object.values(lopt).map((v) => {
+            lcompid = {};
+            Object.values(lopt).forEach((v) => {
                 let [param, result, opt, tag] = v;
                 let [ptag, rtag] = [param, result].map(jsonify);
                 lresult[rtag] = lresult[rtag] || [];
@@ -78,9 +86,21 @@ class RouterList {
                     lparam[v[0]].push([tag, v[1]]);
                 });
             })
+            let bindcomp = (v) => {
+                if (lcompid[v] !== undefined) return;
+                let ty = t.type.parse(JSON.parse(v));
+                if (ty.meta.type == "raw") { lcompid[v] = lcomp[v]; return }
+                lcompid[v] = lgeneric[ty.meta.type.name].map((v, i) => {
+                    let ptag = jsonify(ty.meta.param);
+                    if (lcompid[ptag] === undefined) bindcomp(ptag);
+                    v(lcompid[ptag][i]);
+                });
+            }
+            Object.keys(lresult).forEach(bindcomp);
+            Object.keys(lparam).forEach(bindcomp);
             changed = false;
         }
-        let output = (v, tp) => new Router(lopt[v[0]], lcomp, v[1], tp);
+        let output = (v, tp) => new Router(lopt[v[0]], lcompid, v[1], tp);
         this.want = (what) => {
             t.type.assert(what);
             this.update();
