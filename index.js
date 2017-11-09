479ff891bcd5b389d71ba57323698c4fa3777007
hjiayz <hjiayz@163.com>
2017-11-09 16:23:00 +0800

    get_type

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ class RouterList {
             return this;
         }
         this.get = (opt_tag) => lopt[opt_tag];
+        this.get_type = (type, isview) => lcompid[jsonify(type)][isview | 0]
         this.update = () => {
             if (!changed) return;
             lresult = {};
